a742e4fccb888ed51360c956d0be226bfbdaa71a
Rogue Blue <rogueblueprint@protonmail.com>
2022-03-09 17:19:58 -0700

    Added Solution For Problem 2

--- Unit-1/problem-2.py
+++ Unit-1/problem-2.py
@@ -3,3 +3,13 @@
 # For example, if s = 'azcbobobegghakl', 
 # then your program should print:
     # Number of times bob occurs is: 2
+
+
+s = 'azcbobobegghakl'
+bob_count = 0
+
+for x in range(len(s) - 2):
+    if s[x] == 'b' and s[x + 1] == 'o' and s[x + 2] == 'b':
+        bob_count+=1
+
+print(f'Number of times bob occurs is: {bob_count}')
