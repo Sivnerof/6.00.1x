8b0b55ea449c5684bb4c3408ef5ddf50e7c64b56
Rogue Blue <rogueblueprint@protonmail.com>
2022-03-09 17:00:10 -0700

    Added Solution For Problem 1

--- Unit-1/problem-1.py
+++ Unit-1/problem-1.py
@@ -5,3 +5,12 @@
 # your program should print:
     # Number of vowels: 5
 
+s = 'azcbobobegghakl'
+vowels = ['a', 'e', 'i', 'o', 'u']
+vowel_count = 0
+
+for letter in s:
+    if letter in vowels:
+        vowel_count += 1
+
+print(f'Number of vowels: {vowel_count}')
