d1dbedee9d411317b91034dbfb92f6fa91efce43
Rogue Blue <rogueblueprint@protonmail.com>
2022-03-12 00:16:26 -0700

    Deleted Comments In Code

--- Unit-2/Secret-Guesses/guesses.py
+++ Unit-2/Secret-Guesses/guesses.py
@@ -1,12 +1,5 @@
-# Please think of a number between 0 and 100!
-# Is your secret number 50?
-# Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. l
-# Is your secret number 75?
-# Game over. Your secret number was: 83
-# Sorry, I did not understand your input.
-
 try:
-    secret_number = int(input('Enter A Number Between 0-100 And I Will Guess It: '))
+    secret_number = int(input('Please think of a number between 0 and 100! '))
 except:
     print('Must be an integer')
 
@@ -19,12 +12,19 @@ low_point = 0
 while(True):
     guess = None
     mid_point = int((high_point + low_point) / 2)
+    
     while(True):
-        guess = input(f'Is your secret number {mid_point}? ')
+        print(f'Is your secret number {mid_point}? ')
+        print("Enter 'h' to indicate the guess is too high.", end='') 
+        print("Enter 'l' to indicate the guess is too low.", end='') 
+        print("Enter 'c' to indicate I guessed correctly.", end='')
+        guess = input(' ')
+
         if guess == 'l' or guess == 'h' or guess =='c':
             break
         else:
             print('Sorry, I did not understand your input.')
+    
     if guess == 'l':
         low_point = mid_point
     elif guess == 'h':
@@ -32,6 +32,3 @@ while(True):
     elif guess == 'c':
         print(f'Game over. Your secret number was: {mid_point}')
         break
-    print(f'high point = {high_point}')
-    print(f'low point = {low_point}')
-    print(f'mid point = {mid_point}')
